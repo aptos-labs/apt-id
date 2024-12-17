c70161b178a6920c811229a1f6c5d0b539457709
Greg Nazario <greg@gnazar.io>
2024-12-17 14:59:35 -0500

    Update deps again (#13)
    * update eslint deps
    
    * Upgrade to latest react / next, remove unused dependencies
    
    * more updates

--- typescript/src/app/[name]/page.tsx
+++ typescript/src/app/[name]/page.tsx
@@ -2,12 +2,13 @@ import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
 import ProfileClient from "./ProfileClient";
 import { CONTRACT_ADDRESS } from "@/constants.ts";
 import { redirect } from 'next/navigation';
+import NotFound from "@/app/not-found.tsx";
 
 type ImageBio = { __variant__: "Image"; avatar_url: string; bio: string; name: string };
 type NFTBio = { __variant__: "NFT"; nft_url: { inner: string }; bio: string; name: string };
 type LinkTree = {
   __variant__: "SM";
-  links: { data: { key: string; value: { "__variant__": "UnorderedLink"; "url": string } }[] };
+  links: { data: { key: string; value: { __variant__: "UnorderedLink"; url: string } }[] };
 };
 
 async function getServerState() {
@@ -24,11 +25,14 @@ export async function generateStaticParams() {
 }
 
 interface PageProps {
-  params: { name: string };
+  params: Promise<{ name: string }>;
 }
 
 export default async function ProfilePage(props: PageProps) {
-  const { params } = props;
+  const params = await props.params;
+  if (!params?.name) {
+    return <NotFound />;
+  }
 
   // Redirect .apt URLs to base name
   if (params.name.endsWith('.apt')) {
@@ -44,43 +48,54 @@ export default async function ProfilePage(props: PageProps) {
   const address = await state.mainnetClient.ans.getTargetAddress({ name: lookupName })
     .catch(() => undefined);
 
-  const bio = address ? await state.client.view<[{ vec: [ImageBio | NFTBio] }]>({
-    payload: {
-      function: `${CONTRACT_ADDRESS}::profile::view_bio`,
-      functionArguments: [address],
-    },
-  }).then(([data]) => {
-    const bio = data.vec[0];
-    // TODO: Lookup avatar_url for NFT
-    if (bio.__variant__ === "Image") {
-      return {
-        name: bio.name,
-        bio: bio.bio,
-        avatar_url: bio.avatar_url ?? "NFT Image",
-      };
-    } else {
-      return {
-        name: bio.name,
-        bio: bio.bio,
-        avatar_url: "NFT Image",
-      };
-    }
-  }).catch(() => undefined) : undefined;
+  const bio = address
+    ? await state.client
+        .view<[{ vec: [ImageBio | NFTBio] }]>({
+          payload: {
+            function: `${CONTRACT_ADDRESS}::profile::view_bio`,
+            functionArguments: [address],
+          },
+        })
+        .then(([data]) => {
+          const bio = data.vec[0];
+          // TODO: Lookup avatar_url for NFT
+          if (bio.__variant__ === "Image") {
+            return {
+              name: bio.name,
+              bio: bio.bio,
+              avatar_url: bio.avatar_url ?? "NFT Image",
+            };
+          } else {
+            return {
+              name: bio.name,
+              bio: bio.bio,
+              avatar_url: "NFT Image",
+            };
+          }
+        })
+        .catch(() => undefined)
+    : undefined;
 
-  const links = address ? await state.client.view<[LinkTree]>({
-    payload: {
-      function: `${CONTRACT_ADDRESS}::profile::view_links`,
-      functionArguments: [address],
-    },
-  }).then(([data]) => {
-    const inner = data?.links?.data?.map((link) => ({
-      id: link.key,
-      title: link.key,
-      url: link.value.url,
-    })) ?? [];
+  const links = address
+    ? await state.client
+        .view<[LinkTree]>({
+          payload: {
+            function: `${CONTRACT_ADDRESS}::profile::view_links`,
+            functionArguments: [address],
+          },
+        })
+        .then(([data]) => {
+          const inner =
+            data?.links?.data?.map((link) => ({
+              id: link.key,
+              title: link.key,
+              url: link.value.url,
+            })) ?? [];
 
-    return inner;
-  }).catch(() => undefined) : undefined;
+          return inner;
+        })
+        .catch(() => undefined)
+    : undefined;
 
   // Create a profile using the URL parameter as the ANS name
   const profile = {
