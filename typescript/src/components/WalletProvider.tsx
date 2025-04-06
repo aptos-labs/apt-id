c2d79fcaceef9854e06ad788dafbef1bb0cdc769
Greg Nazario <greg@gnazar.io>
2025-04-06 13:41:56 -0400

    [deps] Upgrade wallet adapter and axios

--- typescript/src/components/WalletProvider.tsx
+++ typescript/src/components/WalletProvider.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
-import { PetraWallet } from "petra-plugin-wallet-adapter";
 import { ReactNode } from "react";
 
 export function WalletProvider({ children }: { children: ReactNode }) {
-  const wallets = [new PetraWallet()];
-
   return (
-    <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
+    <AptosWalletAdapterProvider autoConnect={true}>
       {children}
     </AptosWalletAdapterProvider>
   );
